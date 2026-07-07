a442d57d738b0ffbd16094eeea1441eaeb4a8c52
BatchuUmaNagaSriAkhil <23ht1a4407@gmail.com>
2026-07-07 10:26:43 +0530

    Add files via upload

---
+++ problem2.py
@@ -0,0 +1,19 @@
+s = input()
+
+num = ""
+max_num = -1
+
+for ch in s:
+    if ch.isdigit():
+        num += ch
+    else:
+        if num:
+            if '9' not in num:
+                max_num = max(max_num, int(num))
+            num = ""
+
+if num:
+    if '9' not in num:
+        max_num = max(max_num, int(num))
+
+print(max_num)
