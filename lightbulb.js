40fb781a0032f86e2b3b48edf5ba29e55baf8454
BatchuUmaNagaSriAkhil <23ht1a4407@gmail.com>
2026-07-07 10:32:26 +0530

    Add files via upload

---
+++ lightbulb.js
@@ -0,0 +1,16 @@
+let lightbulb = document.getElementById("lightbulb");
+let onButton = document.getElementById("onButton");
+let offButton = document.getElementById("offButton");
+let isOn = false;
+
+onButton.addEventListener("click", function() {
+    isOn = true;
+    lightbulb.style.borderColor = "yellow";
+    lightbulb.style.backgroundColor = "lightyellow";
+});
+
+offButton.addEventListener("click", function() {
+    isOn = false;
+    lightbulb.style.borderColor = "black";
+    lightbulb.style.backgroundColor = "white";
+});
