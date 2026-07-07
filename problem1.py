417c513145018284d302830d3d5d428a03acc6ca
BatchuUmaNagaSriAkhil <23ht1a4407@gmail.com>
2026-07-07 10:28:06 +0530

    Add files via upload

---
+++ problem1.py
@@ -0,0 +1,17 @@
+def unique_digits(num):
+    s = str(num)
+    return len(s) == len(set(s))
+
+L = int(input())
+R = int(input())
+
+count = 0
+
+for i in range(L, R + 1):
+    if unique_digits(i):
+        count += 1
+
+if count == 0:
+    print("NoUnique Number")
+else:
+    print(count)
